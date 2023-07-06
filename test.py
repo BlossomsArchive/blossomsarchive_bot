cb49dda0f718da8ce4f81aab4fe152d447825dd3
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-06 15:03:06 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,8 @@ now = datetime.datetime.now()
 f2 = open("test.txt", "a")
 f2.write(str(now)+"\n")
 f2.close
+
+#Tue, 27 Jun 2023 14:00:07 +0000
+f2 = open("feed.txt", "a")
+f2.write(str(now)+"\n")
+f2.close
