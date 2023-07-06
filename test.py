a3ab114d466de524a989edbc5e259f0aebb78a70
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-06 15:06:15 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ f2 = open("post-list.txt", "a")
 f2.write(str(now)+"\n")
 f2.close
 
-#Tue, 27 Jun 2023 14:00:07 +0000
-f2 = open("feed.txt", "a")
-f2.write(str(now)+"\n")
+f2 = open("feed.txt", "w")
+f2.write("Tue, 27 Jun 2023 14:00:07 +0000")
 f2.close
