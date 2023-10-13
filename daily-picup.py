fe2214acab35c0011a975dd721928e42f4cd353b
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-10-13 23:18:26 +0900

    Update daily-picup.py

--- daily-picup.py
+++ daily-picup.py
@@ -51,7 +51,14 @@ api.toot(post_text)
 #bluesky
 bluesky = Client()
 bluesky.login(str(os.environ.get("BLUESKY_MAIL_ADDRESS")),str(os.environ.get("BLUESKY_PASSWORD")))
-bluesky.send_post(post_text)
+embed_external = models.AppBskyEmbedExternal.Main(
+    external = models.AppBskyEmbedExternal.External(
+        title = title,
+        description = "BlossomsArchive",
+        uri = page_url
+    )
+)
+bluesky.send_post("【本日のおすすめ記事】"+"\n"+title ,embed = embed_external)
 
 # Twitter
 twitter_bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
