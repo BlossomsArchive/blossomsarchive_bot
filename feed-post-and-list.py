f9662aa36ad93ca531f1313b054853679f00a3af
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-10-14 23:12:41 +0900

    Add files via upload

--- feed-post-and-list.py
+++ feed-post-and-list.py
@@ -1,11 +1,13 @@
 # coding: utf-8
-
-import os
-import feedparser
 from misskey import Misskey
 import tweepy
 from mastodon import Mastodon
-from atproto import Client ,models
+import os
+from atproto import Client, models
+import threading
+import time
+import feedparser
+
 
 f = open("feed.txt", "r")
 old_up = f.read()
@@ -25,9 +27,7 @@ while True:
     now_entry = entries[i]
     if now_entry["updated"] == old_up:
         new_up = entries[0]["updated"]
-        f3 = open(
-            "feed.txt", "w"
-        )
+        f3 = open("feed.txt", "w")
         f3.write(new_up)
         f3.close()
         break
@@ -56,55 +56,130 @@ while True:
 
         post_text = "【" + author + "がブログを更新しました】\n" + title + "\n" + page_url
 
+
+    # スレッドが終了したことを追跡するためのリスト
+    threads = []
+
+
+    # 各スレッドで実行する関数
+    def thread_function(thread_id):
         # Misskey
-        misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
-        misskey_token = os.environ.get("MISSKEY_TOKEN")
-        api = Misskey(misskey_address)
-        api.token = misskey_token
-        api.notes_create(text=post_text)
+        if thread_id == 1:
+            # 処理内容
+            a = 1
+            while True:
+                try:
+                    if a != 20:
+                        api = Misskey(os.environ.get("MISSKEY_SERVER_ADDRESS"))
+                        api.token = os.environ.get("MISSKEY_TOKEN")
+                        api.notes_create(text=post_text)
+                    else:
+                        break
+                except:
+                    print(f"Misskey - Result: NO")
+                    print(f"Misskey - ReTry: {a}")
+                    a = a + 1
+                    time.sleep(300)
+                else:
+                    print(f"Misskey - Result: OK")
+                    break
 
         # Mastodon
-        mastdon_url = os.environ.get("MASTDON_BASE_URL")
-        mastdon_CID = os.environ.get("MASTDON_CLIENT_ID")
-        mastdon_secret = os.environ.get("MASTDON_SECRET")
-        mastdon_token = os.environ.get("MASTDON_TOKEN")
-        api = Mastodon(
-            api_base_url=mastdon_url,
-            client_id=mastdon_CID,
-            client_secret=mastdon_secret,
-            access_token=mastdon_token,
-        )
-        api.toot(post_text)
+        elif thread_id == 2:
+            # 処理内容
+            b = 1
+            while True:
+                try:
+                    if b != 20:
+                        api = Mastodon(
+                            api_base_url=os.environ.get("MASTDON_BASE_URL"),
+                            client_id=os.environ.get("MASTDON_CLIENT_ID"),
+                            client_secret=os.environ.get("MASTDON_SECRET"),
+                            access_token=os.environ.get("MASTDON_TOKEN"),
+                        )
+                        api.toot(post_text)
+                    else:
+                        break
+                except:
+                    print(f"Mastdon - Result: NO")
+                    print(f"Mastdon - ReTry: {b}")
+                    b = b + 1
+                    time.sleep(300)
+                else:
+                    print(f"Mastdon - Result: OK")
+                    break
 
-        #bluesky
-        bluesky = Client()
-        bluesky.login(str(os.environ.get("BLUESKY_MAIL_ADDRESS")),str(os.environ.get("BLUESKY_PASSWORD")))
-        embed_external = models.AppBskyEmbedExternal.Main(
-            external = models.AppBskyEmbedExternal.External(
-                title = title,
-                description = "BlossomsArchive",
-                uri = page_url
-            )
-        )
-        bluesky.send_post("【" + author + "がブログを更新しました】\n" + title ,embed = embed_external)
-        
+        # Bluesky
+        elif thread_id == 3:
+            # 処理内容
+            c = 1
+            while True:
+                try:
+                    if c != 20:
+                        bluesky = Client()
+                        bluesky.login(
+                            str(os.environ.get("BLUESKY_MAIL_ADDRESS")),
+                            str(os.environ.get("BLUESKY_PASSWORD")),
+                        )
+                        embed_external = models.AppBskyEmbedExternal.Main(
+                            external=models.AppBskyEmbedExternal.External(
+                                title=title, description="BlossomsArchive", uri=page_url
+                            )
+                        )
+                        bluesky.send_post("【" + author + "がブログを更新しました】\n" + title ,embed = embed_external)
+
+                    else:
+                        break
+                except:
+                    print(f"Bluesky - Result: NO")
+                    print(f"Bluesky - ReTry: {c}")
+                    c = c + 1
+                    time.sleep(300)
+                else:
+                    print(f"Bluesky - Result: OK")
+                    break
+
         # Twitter
-        twitter_bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
-        twitter_consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
-        twitter_consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
-        twitter_access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
-        twitter_access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+        elif thread_id == 4:
+            # 処理内容
+            d = 1
+            while True:
+                try:
+                    if d != 20:
+                        client = tweepy.Client(
+                            bearer_token=os.environ.get("TWITTER_BEARER_TOKEN"),
+                            consumer_key=os.environ.get("TWITTER_CONSUMER_KEY"),
+                            consumer_secret=os.environ.get("TWITTER_CONSUMER_SECRET"),
+                            access_token=os.environ.get("TWITTER_ACCESS_TOKEN"),
+                            access_token_secret=os.environ.get(
+                                "TWITTER_ACCESS_TOKEN_SECRET"
+                            ),
+                        )
+                        client.create_tweet(text=post_text)
+                    else:
+                        break
+                except:
+                    print(f"Twitter - Result: NO")
+                    print(f"Twitter - ReTry: {d}")
+                    d = d + 1
+                    time.sleep(300)
+                else:
+                    print(f"Twitter - Result: OK")
+                    break
 
-        client = tweepy.Client(
-            bearer_token=twitter_bearer_token,
-            consumer_key=twitter_consumer_key,
-            consumer_secret=twitter_consumer_secret,
-            access_token=twitter_access_token,
-            access_token_secret=twitter_access_token_secret,
-        )
 
-        client.create_tweet(text=post_text)
+    # 4つのスレッドを作成し、それぞれ異なる関数を実行
+    for i in range(1, 5):
+        thread = threading.Thread(target=thread_function, args=(i,))
+        threads.append(thread)
+        thread.start()
 
-    print(post_text+"\n")
+    # 全てのスレッドが終了するのを待つ
+    for thread in threads:
+        thread.join()
 
+    # すべてのスレッドが終了した後にメッセージを表示
+    print("\n" + post_text + "\n")
     i += 1
+        
+print("All End")
